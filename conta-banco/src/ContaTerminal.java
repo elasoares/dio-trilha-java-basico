b1d35589245ada00649c0c938e64c449356d56c2
Elaine <elaine.almeida@al.infnet.edu.br>
2024-05-14 08:59:11 -0300

    Commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        int numero = 1;
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da sua Agência: ");
+        int agencia = entrada.nextInt();
+        System.out.println("Por favor, digite o número da sua Conta: ");
+        int numero = entrada.nextInt();
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = entrada.next();
+        System.out.println("Por favor, digite o seu saldo: ");
+        double saldo = entrada.nextDouble();
+
+        System.out.println("Olá " + nome + ", " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", " + "conta " + numero + " e seu saldo " + saldo +","+ " já estão disponível para saque.");
+
+        entrada.close(); 
     }
 }
